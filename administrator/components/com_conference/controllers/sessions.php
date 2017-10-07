fb274309cde372fa0ad5cae26dba16cb45781eb3
Roland Dalmulder <contact@rolandd.com>
2017-10-07 15:28:44 +0200

    Updating language strings and year placeholder

--- administrator/components/com_conference/controllers/sessions.php
+++ administrator/components/com_conference/controllers/sessions.php
@@ -3,7 +3,7 @@
  * @package     Conference
  *
  * @author      Stichting Sympathy <info@stichtingsympathy.nl>
- * @copyright   Copyright (C) 2013 - @year@ Stichting Sympathy. All rights reserved.
+ * @copyright   Copyright (C) 2013 - [year] Stichting Sympathy. All rights reserved.
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  * @link        https://joomladagen.nl
  */
@@ -17,6 +17,14 @@ defined('_JEXEC') or die;
  */
 class ConferenceControllerSessions extends JControllerAdmin
 {
+	/**
+	 * The prefix to use with controller messages.
+	 *
+	 * @var    string
+	 * @since  1.0
+	 */
+	protected $text_prefix = 'COM_CONFERENCE_SESSION';
+
 	/**
 	 * Proxy for getModel.
 	 *
--- administrator/components/com_conference/models/rooms.php
+++ administrator/components/com_conference/models/rooms.php
@@ -4,7 +4,7 @@
  * @package     Conference
  *
  * @author      Stichting Sympathy <info@stichtingsympathy.nl>
- * @copyright   Copyright (C) 2013 - @year@ Stichting Sympathy. All rights reserved.
+ * @copyright   Copyright (C) 2013 - [year] Stichting Sympathy. All rights reserved.
  * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
  * @link        https://joomladagen.nl
  */
